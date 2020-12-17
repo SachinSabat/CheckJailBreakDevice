0c4a27c831b5b98a0c1d43a78639fb4e2fbcac81
Sachin Sabat <sabat.sachin33@gmail.com>
2020-12-17 23:26:07 +0530

    Intital project setup

---
+++ CheckJailBreakDevice/CheckJailBreakDevice.h
@@ -0,0 +1,18 @@
+//
+//  CheckJailBreakDevice.h
+//  CheckJailBreakDevice
+//
+//  Created by Sachin Sabat on 17/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckJailBreakDevice.
+FOUNDATION_EXPORT double CheckJailBreakDeviceVersionNumber;
+
+//! Project version string for CheckJailBreakDevice.
+FOUNDATION_EXPORT const unsigned char CheckJailBreakDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckJailBreakDevice/PublicHeader.h>
+
+
---
+++ CheckJailBreakDeviceExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckJailBreakDeviceExample
+//
+//  Created by Sachin Sabat on 17/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
